b3a5c575e45c100a2940c27059e390c572b7ad02
romanfilimonchik <lekardre@mail.ru>
2016-09-02 21:08:35 +0300

    jd_01_06

--- src/by/it/filimonchik/jd_01_06/jd_01_06/TaskA3.java
+++ src/by/it/filimonchik/jd_01_06/jd_01_06/TaskA3.java
@@ -5,6 +5,13 @@ package by.it.filimonchik.jd_01_06.jd_01_06;
  */
 public class TaskA3 {
     static boolean Glasnaya(String word){
-        String oae ="яыуаиеоюэ"
+        String oae ="яыуаиеоюэё";
+        oae=oae+oae.toUpperCase();
+        char first=word.charAt(0);
+        char last=word.charAt(word.length()-1);
+        return (oae.indexOf(first)>0)&& oae.indexOf(last)>0;
+    }
+    public static void main (String [] args){
+
     }
 }
