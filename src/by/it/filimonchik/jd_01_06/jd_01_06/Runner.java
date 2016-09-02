4489dcc69c2e62f0f891cb60ab6ba965d8b7ae73
romanfilimonchik <lekardre@mail.ru>
2016-09-02 20:57:46 +0300

    jd_01_06

---
+++ src/by/it/filimonchik/jd_01_06/jd_01_06/Runner.java
@@ -0,0 +1,16 @@
+package by.it.filimonchik.jd_01_06.jd_01_06;
+
+import by.it.filimonchik.jd_01_06.jd_01_06.Data;
+import by.it.filimonchik.jd_01_06.jd_01_06.TaskA1;
+
+/**
+ * Created by user on 02.09.2016.
+ */
+public class Runner {
+    public static void main(String[] args){
+//        System.out.println(Data.luk);
+//        String res= TaskA1.z1(Data.luk);
+//        System.out.println(res);
+        TaskA1.z2(Data.luk);
+    }
+}
