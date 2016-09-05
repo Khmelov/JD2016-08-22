f1eeb4512682b42e7231936719dc48a7bd2f2dc0
opiga <bondarolga1994@gmail.com>
2016-09-05 21:16:38 +0300

    JD05-Task6

---
+++ src/by/it/opiga/JD01_05/Task6.java
@@ -0,0 +1,31 @@
+package by.it.opiga.JD01_05;
+
+/**
+ * Created by user on 05.09.2016.
+ */
+public class Task6 {
+
+    public static void fullMas()
+    {
+        double f1=-8;
+        double f2=-2;
+        double step=0.2;
+        double a[];
+        int col=0;
+        System.out.println((f2-f1)/0.2);
+        for (double i = f1; i <f2 ; f1++) {
+            System.out.println(f1);
+            //a[col]=
+            f1=f1+step;
+
+
+
+        }
+
+    }
+
+
+
+
+
+}
