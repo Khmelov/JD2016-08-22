04cbfa23cc001841c9313de284f29a151de858dc
aborisenok <lesha_borisenok@mail.ru>
2016-09-06 01:10:59 +0300

    Работаем

--- src/by/it/aborisenok/JD01_02/TaskB.java
+++ src/by/it/aborisenok/JD01_02/TaskB.java
@@ -8,19 +8,17 @@ import java.io.IOException;
 public class TaskB {
     //принадлежит ли число интервалу
     public static void ifInterval() throws IOException {
-        System.out.println("Введите начало интервала");
-        String sBegin = Util.getOneLine();
-        int begin = Integer.parseInt(sBegin);
-        System.out.println("Введите конец интервала");
-        int end = Util.lineToNumber(Util.getOneLine());
-        System.out.println("Введите искомое значение");
-        int value = Util.lineToNumber(Util.getOneLine());
-
-        if ((value >= begin) && (value <= end) ) {
-            System.out.printf("Значение %d принадлежит интервалу [%d, %d]", value, begin, end);
+        System.out.println("Введите через пробел начало интервала m, конец интервала n, и значение value");
+        String line = Util.getOneLine();
+        int[] param = Util.lineToIntArray(line);
+        int m = param[0];
+        int n = param[1];
+        int value = param[2];
+        if ((value >= m) && (value <= n) ) {
+            System.out.printf("Значение %d принадлежит интервалу [%d, %d]", value, m, n);
         }
         else {
-            System.out.printf("Значение %d принадлежит интервалу [%d, %d]", value, begin, end);
+            System.out.printf("Значение %d не принадлежит интервалу [%d, %d]", value, m, n);
         }
     }
 
@@ -28,14 +26,15 @@ public class TaskB {
 
     // Найти корни квадратного уравнения
     public static void quadraticEquation() throws IOException {
-        System.out.println("Введите коэффициент a");
-        int a = Util.lineToNumber(Util.getOneLine());
-        System.out.println("Введите коэффициент b");
-        int b = Util.lineToNumber(Util.getOneLine());
-        System.out.println("Введите коэффициент c");
-        int c = Util.lineToNumber(Util.getOneLine());
+        System.out.println("Введите через пробел коэффициенты [a], [b], [c]");
+        String line = Util.getOneLine();
+        int[] koef = Util.lineToIntArray(line);
+        int a = koef[0];
+        int b = koef[1];
+        int c = koef[2];
 
-        int dis = b^2 - 4*a*c;
+        int dis = b*b - 4*a*c;
+        System.out.println(dis);
         if (dis < 0) {
             System.out.println("Уравнение не имеет корней");
         } else
@@ -46,7 +45,7 @@ public class TaskB {
                 if (dis > 0) {
                     double x1 = (-b + Math.sqrt(dis))/(2*a);
                     double x2 = (-b - Math.sqrt(dis))/(2*a);
-                    System.out.println("Уравнение имеет два корня х1 = " + x1 +  "x2 = " + x2);
+                    System.out.println("Уравнение имеет два корня х1 = " + x1 + " x2 = " + x2);
                 }
     }
 }
--- src/by/it/aborisenok/JD01_02/Util.java
+++ src/by/it/aborisenok/JD01_02/Util.java
@@ -9,21 +9,14 @@ import java.io.InputStreamReader;
  */
 public class Util {
     //чтение одной строки с консоли
-    static String getOneLine() throws IOException {
-        BufferedReader stin = new BufferedReader( new InputStreamReader(System.in));
-        String line = stin.readLine();
-        stin.close();
+   public static String getOneLine() throws IOException {
+        BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
+        String line = reader.readLine();
         return line;
     }
 
-    //преобразование строки в число
-    static int lineToNumber(String line) {
-        int number = Integer.parseInt(line);
-        return number;
-    }
-
     //преобразование одной строки в массив чисел
-    static int[] lineToIntArray(String line){
+   public static int[] lineToIntArray(String line){
         //деление строки на элементы массива по пробелам
         String[] strArray = line.split(" ");
         //создаем и заполняем массив чисел
