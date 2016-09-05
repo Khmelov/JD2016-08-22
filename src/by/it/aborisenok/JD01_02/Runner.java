9a33b5dc9c3efae1c93ccc50c73f550aa0f251dd
aborisenok <lesha_borisenok@mail.ru>
2016-09-06 01:10:59 +0300

    Работаем

--- src/by/it/aborisenok/JD01_02/Runner.java
+++ src/by/it/aborisenok/JD01_02/Runner.java
@@ -4,42 +4,41 @@ import java.io.IOException;
 
 public class Runner {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         //прочитаем линию значений с клавиатуры.
-       // String  line = Util.getOneLine();
-//        String  line = "111 22 343 123 67 4 33 22 834 66";
-//        //получим массив чисел с которым будем работать:
-//        int arr[] = Util.lineToIntArray(line);
-//
-//        //A.1 Найти самое короткое и самое длинное число.
-//        //   Вывести найденные числа и их длину.
-//        String max = TaskA.getMax(arr);
-//        System.out.println(max);
-//        String min = TaskA.getMin(arr);
-//        System.out.println(min);
-//
-//        //A.2 Вывести на консоль те числа, длина которых меньше
-//        //   (больше) средней.
-//        double mean=TaskA.mean(arr);
-//        System.out.print("Числа меньше "+mean+" таковы: ");
-//        for (int i:arr) {
-//            if (i<mean) {
-//                System.out.print(i+" ");
-//            }
-//        }
-//        System.out.println();
-//
-//        //A.3 Найти число, состоящее только из различных цифр.
-//        //Если таких чисел несколько, найти первое из них.
-//        for (int i:arr) {
-//            if (TaskA.uniqueDigits(i)) {
-//                System.out.print("Найдено число из разных цифр: "+i);
-//                break; //больше ничего не ищем
-//            }
-//        }
-//
-//        System.out.println("-------------------Часть Б---------------------");
+        System.out.println("Введите с клавиатуры сроку цифр");
+        String  line = Util.getOneLine();
+        //String  line = "111 22 343 123 67 4 33 22 834 66";
+        //получим массив чисел с которым будем работать:
+        int arr[] = Util.lineToIntArray(line);
 
+        //A.1 Найти самое короткое и самое длинное число.
+        //   Вывести найденные числа и их длину.
+        String max = TaskA.getMax(arr);
+        System.out.println(max);
+        String min = TaskA.getMin(arr);
+        System.out.println(min);
+
+        //A.2 Вывести на консоль те числа, длина которых меньше
+        //   (больше) средней.
+        double mean=TaskA.mean(arr);
+        System.out.print("Числа меньше "+ mean +" таковы: ");
+        for (int i:arr) {
+            if (i<mean) {
+                System.out.print(i+" ");
+            }
+        }
+        System.out.println();
+
+        //A.3 Найти число, состоящее только из различных цифр.
+        //Если таких чисел несколько, найти первое из них.
+        for (int i:arr) {
+            if (TaskA.uniqueDigits(i)) {
+                System.out.println("Найдено число из разных цифр: "+i);
+                break; //больше ничего не ищем
+            }
+        }
+        //
         //Б.1 Определить принадлежит ли значение k интервалу [m, n]
         TaskB.ifInterval();
 
@@ -47,6 +46,6 @@ public class Runner {
         // (k выберите квадратом целого числа).
 
         //Б.3 Найти корни квадратного уравнения
-       // TaskB.quadraticEquation();
+        TaskB.quadraticEquation();
     }
 }
