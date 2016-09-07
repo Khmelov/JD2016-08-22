050dfba060a346d40da374dc3f6e663a1d943133
aborisenok <lesha_borisenok@mail.ru>
2016-09-07 14:04:45 +0300

    Работаем

---
+++ src/by/it/aborisenok/JD01_06/TaskB.java
@@ -0,0 +1,41 @@
+package by.it.aborisenok.JD01_06;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Лёша on 07.09.2016.
+ */
+public class TaskB {
+    // Task 1
+    public static void deletWords(String text){
+        StringBuilder string = new StringBuilder(text);
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]{5,}");
+        Matcher matcher = pattern.matcher(string);
+
+        while (matcher.find()) {
+            int first = matcher.start();
+            int end = matcher.end();
+            int length = matcher.group().length(); //тут длина найденого слова
+
+            if ((length == 5) && isWord(matcher.group())){
+                System.out.println(matcher.group());
+                string.delete(first, end);
+
+            }
+        }
+
+    }
+
+   private static boolean isWord(String word){
+        String consonants = "бвгджзйклмнпрстфхцчшщ";
+        consonants = consonants + consonants.toUpperCase();
+        char first = word.charAt(0);
+        //вернем true если согласная в начале
+        return (consonants.indexOf(first) >= 0);
+    }
+}
+
+
+// \b\w{3,}\b  слова 3 и более букв
