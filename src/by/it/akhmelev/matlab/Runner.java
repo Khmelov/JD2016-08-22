bb9d03125f9ca743a9ba76b3f20cca386535bbe3
akhmelev <zaokhm0ok>
2016-09-09 20:41:51 +0300

    matlab

--- src/by/it/akhmelev/matlab/Runner.java
+++ src/by/it/akhmelev/matlab/Runner.java
@@ -11,7 +11,15 @@ public class Runner {
         Var v2=f1.add(v1);
         System.out.println("Сумма скаляра и вектора: "+v2);
 
+        Var v3=v1.add(f2);
+        System.out.println("Сумма вектора и скаляра: "+v3);
 
+        Var v4=v1.add(v1);
+        System.out.println("Сумма двух векторов: "+v4);
+
+        Var v5=new VarV(new double[]{1,2});
+        Var v6=v1.add(v5);
+        System.out.println("Сумма двух разных (по размеру) векторов: "+v6);
 
 
     }
