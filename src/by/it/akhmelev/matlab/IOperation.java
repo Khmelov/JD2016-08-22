f1c475d894b40e28892894c98799a490301c139a
akhmelev <zaokhm0ok>
2016-09-09 20:31:44 +0300

    Примеры с 06 занятия (регулярные выражения)

---
+++ src/by/it/akhmelev/matlab/IOperation.java
@@ -0,0 +1,11 @@
+package by.it.akhmelev.matlab;
+
+/**
+ * Created by user on 09.09.2016.
+ */
+public interface IOperation {
+    Var add(Var v);
+    Var sub(Var v);
+    Var mul(Var v);
+    Var div(Var v);
+}
---
+++ src/by/it/akhmelev/matlab/Var.java
@@ -0,0 +1,43 @@
+package by.it.akhmelev.matlab;
+
+/**
+ * Created by user on 09.09.2016.
+ */
+public abstract class Var implements IOperation{
+    @Override
+    public Var add(Var v) {
+        if (v instanceof VarF) return add((VarF) v);
+        if (v instanceof VarV) return add((VarV) v);
+        if (v instanceof VarM) return add((VarM) v);
+        return null;
+    }
+
+    public Var add(VarF v) {
+        Log.print("Сложение со скаляром невозможна");
+        return null;
+    }
+    public Var add(VarV v) {
+        Log.print("Сложение с вектором невозможна");
+        return null;
+    }
+    public Var add(VarM v) {
+        Log.print("Сложение с матрицей невозможна");
+        return null;
+    }
+
+
+    @Override
+    public Var sub(Var v) {
+        return null;
+    }
+
+    @Override
+    public Var mul(Var v) {
+        return null;
+    }
+
+    @Override
+    public Var div(Var v) {
+        return null;
+    }
+}
