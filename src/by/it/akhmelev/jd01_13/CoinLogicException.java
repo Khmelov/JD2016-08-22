b4935a79362c2d6baebbba6a8b1344b0aa6f59ef
akhmelev <zaokhm0ok>
2016-09-19 20:26:58 +0300

    demo jd01_12

---
+++ src/by/it/akhmelev/jd01_13/CoinLogicException.java
@@ -0,0 +1,16 @@
+package by.it.akhmelev.jd01_13;
+
+public class CoinLogicException  extends Exception {
+    //нужно 4 конструктора для своего исключения
+    public CoinLogicException() {
+    }
+    public CoinLogicException(String message, Throwable exception) {
+        super(message, exception);
+    }
+    public CoinLogicException(String message) {
+        super(message);
+    }
+    public CoinLogicException (Throwable exception) {
+        super(exception);
+    }
+}
