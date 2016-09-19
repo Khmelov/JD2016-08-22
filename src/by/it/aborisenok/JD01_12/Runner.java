336b851f4694dcf24e71f158a9ed3357421dd10d
aborisenok <lesha_borisenok@mail.ru>
2016-09-19 20:27:24 +0300

    Задание 12 часть А и В

--- src/by/it/aborisenok/JD01_12/Runner.java
+++ src/by/it/aborisenok/JD01_12/Runner.java
@@ -1,6 +1,10 @@
 package by.it.aborisenok.JD01_12;
 
-import java.util.ArrayList;
+import by.it.aborisenok.JD01_12.TaskA.TaskA1;
+import by.it.aborisenok.JD01_12.TaskA.TaskA2;
+import by.it.aborisenok.JD01_12.TaskA.TaskA3;
+import by.it.aborisenok.JD01_12.TaskB.TaskB1;
+import by.it.aborisenok.JD01_12.TaskB.TaskB2;
 
 /**
  * Created by Лёша on 16.09.2016.
@@ -12,5 +16,11 @@ public class Runner {
         // Task A2/////////////
         TaskA2 A2 = new TaskA2();
         // Task A3////////////
+        TaskA3 A3 = new TaskA3();
+        // Task B1///////////
+        TaskB1 B1 = new TaskB1();
+        // Task B2//////////
+        TaskB2 B2 = new TaskB2();
+
     }
 }
