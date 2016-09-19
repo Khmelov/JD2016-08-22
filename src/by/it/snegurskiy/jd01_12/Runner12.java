a94e3b6338ae31738ba4831e94fbb954aaa93f3e
snegurskiy.nn <advocate.snn@tut.by>
2016-09-19 20:43:15 +0300

    задание 8, также добавил части из 2,4,6 - тоже проврьте их

--- src/by/it/snegurskiy/jd01_12/Runner12.java
+++ src/by/it/snegurskiy/jd01_12/Runner12.java
@@ -16,9 +16,17 @@ public class Runner12 {
         spisok.put("Ivanova", 0);
         spisok.put("Jevnov", 0);
         spisok.put("Kim", 0);
-        System.out.println(spisok);
         spisok=A12.supplyAssessment(spisok);// проставил оценки рандом
+        ArrayList <Integer> Assessment = new ArrayList<Integer>();
+//        System.out.println("Залание А1");
+//        A12.A1(Assessment);
+//        System.out.println(Assessment);
+//        A12.A11(Assessment);
+//        System.out.println(Assessment);
+        System.out.println("Залание А2");
+//        ArrayList <Integer> Assessment = new ArrayList<Integer>();
+//        ArrayList <Integer> Assessment = new ArrayList<Integer>();
 
-        System.out.println(spisok);
+        //System.out.println(spisok);
     }
 }
