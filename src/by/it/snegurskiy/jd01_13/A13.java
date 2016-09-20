5856676d84e0c0c7c05f3e1310f4b143d3a71cab
snegurskiy.nn <advocate.snn@tut.by>
2016-09-20 23:50:12 +0300

    добавил 13 a

---
+++ src/by/it/snegurskiy/jd01_13/A13.java
@@ -0,0 +1,38 @@
+package by.it.snegurskiy.jd01_13;
+
+import java.util.Scanner;
+
+/**
+ * Created by snegurskiy.nn on 20.09.2016.
+ */
+public class A13 {
+
+    public static void A1() throws Exception {
+        double sum=0;
+        for (int i = 0; i <5 ; i++) {
+            try {
+                System.out.println("Введите положительное число");
+                Scanner in = new Scanner(System.in);
+                String s = in.next();
+                if (s.equals("END")) break;
+                try {
+                    int sInt = new Integer(s);
+                } catch (NumberFormatException e) {
+                    System.out.println("введено не число");
+                }
+                int sInt = new Integer(s);
+                if (sInt < 0) throw new Exception13("Введено отрицательное число");
+                sum = sum + Math.sqrt(sInt);
+
+                System.out.println(sInt + " " + sum);
+
+
+            } catch (Exception Exception13) {
+                System.out.println(Exception13);
+            }
+
+
+        }
+        System.out.println("END");
+    }
+}
