6b0ae89bdbb8959f0fa5e5929f7521bfb04eeddf
SpaiR <klimbetoo@gmail.com>
2016-09-20 21:44:23 +0300

    jd01_13 А, В, С. Матлаб во втором коммите.

---
+++ src/by/it/prymshyts/jd01_13/VariantC/Bookcase.java
@@ -0,0 +1,65 @@
+package by.it.prymshyts.jd01_13.VariantC;
+
+/**
+ * Класс наследует все методы и поля абстрактного класса Cupboard.
+ */
+
+class Bookcase extends Cupboard {
+
+    Bookcase() {
+        this.name = "Книжный шкаф";
+    }
+
+    @Override
+    public void showItems() {
+        String bookName = getCorrectSpell(getItems());
+        System.out.println("На полках сейчас " + getItems().toString() + " " + bookName + ".");
+    }
+
+    @Override
+    public void ignite() {
+        super.ignite();
+        setItems(0);
+        System.out.println("Все книги сгорели.");
+    }
+
+    /**
+     * Демонстрация динамического полиморфизма.
+     * Метод вызывает поведение супера и затем определяет дополнительные действия.
+     */
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        setItems(0);
+        System.out.println("Книги разлетелись во все стороны.");
+    }
+
+    @Override
+    public void placeItem() {
+        super.placeItem();
+        System.out.println("На полку положена одна книга.");
+    }
+
+    @Override
+    public void placeItem(Integer itemsNumber) {
+        super.placeItem(itemsNumber);
+        String bookName = getCorrectSpell(itemsNumber);
+        System.out.println("На полку положено " + itemsNumber.toString() + " " + bookName + ".");
+    }
+
+    private String getCorrectSpell(Integer itemsNumber) {
+
+        String correctSpell;
+
+        if (itemsNumber == 1) {
+            correctSpell = "книга";
+        } else if ((itemsNumber > 1 && itemsNumber <= 4) || (itemsNumber > 20 && (itemsNumber % 10 >= 1 && itemsNumber % 10 <= 4))) {
+            correctSpell = "книги";
+        } else {
+            correctSpell = "книг";
+        }
+
+        return correctSpell;
+    }
+}
---
+++ src/by/it/prymshyts/jd01_13/VariantC/Runner.java
@@ -0,0 +1,32 @@
+package by.it.prymshyts.jd01_13.VariantC;
+
+/**
+ * Свой тип исключений - NonexistentItemException.
+ * Пробрасывается в определении метода moveTo, в абстрактном классе Cupboard, в том случае,
+ * если в качестве аргумента приходит 'несуществующий объект' или null.
+ */
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        Cupboard cupboard = new Bookcase();
+
+        try {
+            cupboard.moveTo("стене");
+            cupboard.moveTo("холодильник");
+            cupboard.moveTo("несуществующий предмет");
+        } catch (NonexistentItemException e) {
+            System.err.println(e.getMessage());
+            cupboard.moveTo("прежней позиции");
+        }
+
+        try {
+            cupboard.moveTo("окну");
+            cupboard.moveTo(null);
+        } catch (NonexistentItemException e) {
+            System.err.println(e.getMessage());
+            cupboard.moveTo("прежней позиции");
+        }
+    }
+}
