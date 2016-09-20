e2480958de3ed26b0c5741046a840e2043ee49bb
snegurskiy.nn <advocate.snn@tut.by>
2016-09-20 22:47:58 +0300

    добавил 12 a123b12

--- src/by/it/snegurskiy/jd01_12/Runner12.java
+++ src/by/it/snegurskiy/jd01_12/Runner12.java
@@ -44,22 +44,33 @@ public class Runner12 {
 //        Collections.reverse(setA3);
 //        System.out.println(setA3);
 
-        System.out.println("Залание B1");
-        String s = "Ugh. First day of New Year has been day of horror. Cannot quite believe I am once again starting the year in " +
-                "a single bed in my parents' house. It is too humiliating at my age. I wonder if they'll smell it if I have a " +
-                "fag out of the window. Having skulked at home all day, hoping hangover would clear, I eventually gave up and set " +
-                "off for the Turkey Curry Buffet far too late. When I got to the Alconburys' and rang their entire-tune-of-town-hallclock-style " +
-                "doorbell I was still in a strange world of my own – nauseous, vile-headed, acidic. I was also suffering from road-rage residue after " +
-                "inadvertently getting on to the M6 instead of the M1 and having to drive halfway to Birmingham before " +
-                "I could find anywhere to turn round. I was so furious I kept jamming my foot down to the floor on the accelerator " +
-                "pedal to give vent to my feelings, which is very dangerous. I watched resignedly as Una Alconbury's form – intriguingly " +
-                "deformed through the ripply glass door bore down on me in a fuchsia two-piece.";
-        ArrayList<String> TextS = B12.textInArray(s);
-        HashMap<String, Integer> TextHM = new HashMap();
-        TextHM=B12.B1(TextS);
-        System.out.println(TextHM);
+//        System.out.println("Залание B1");
+//        String s = "Ugh. First day of New Year has been day of horror. Cannot quite believe I am once again starting the year in " +
+//                "a single bed in my parents' house. It is too humiliating at my age. I wonder if they'll smell it if I have a " +
+//                "fag out of the window. Having skulked at home all day, hoping hangover would clear, I eventually gave up and set " +
+//                "off for the Turkey Curry Buffet far too late. When I got to the Alconburys' and rang their entire-tune-of-town-hallclock-style " +
+//                "doorbell I was still in a strange world of my own – nauseous, vile-headed, acidic. I was also suffering from road-rage residue after " +
+//                "inadvertently getting on to the M6 instead of the M1 and having to drive halfway to Birmingham before " +
+//                "I could find anywhere to turn round. I was so furious I kept jamming my foot down to the floor on the accelerator " +
+//                "pedal to give vent to my feelings, which is very dangerous. I watched resignedly as Una Alconbury's form – intriguingly " +
+//                "deformed through the ripply glass door bore down on me in a fuchsia two-piece.";
+//        ArrayList<String> TextS = B12.textInArray(s);
+//        HashMap<String, Integer> TextHM = new HashMap();
+//        TextHM=B12.B1(TextS);
+//        System.out.println(TextHM);
 
         System.out.println("Залание B2");
-
+        ArrayList<Integer> circul1=new ArrayList<>();
+        for (int i = 1; i <=15 ; i++) {
+            circul1.add(i);
+        }
+        LinkedList<Integer> circul2=new LinkedList<>();
+        circul2.addAll(circul1);
+        long timeStart= (long) System.currentTimeMillis();
+        System.out.println(B12.deletecircul(circul1));
+        System.out.println((long) System.currentTimeMillis()-timeStart);
+        timeStart= (long) System.currentTimeMillis();
+        System.out.println(B12.deletecircul(circul2));
+        System.out.println((long) System.currentTimeMillis()-timeStart);
     }
 }
