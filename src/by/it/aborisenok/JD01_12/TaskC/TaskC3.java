7c56c8092836695900c13d93e57b834728860ba2
aborisenok <lesha_borisenok@mail.ru>
2016-09-20 20:01:36 +0300

    Задание 12 А В С

---
+++ src/by/it/aborisenok/JD01_12/TaskC/TaskC3.java
@@ -0,0 +1,63 @@
+package by.it.aborisenok.JD01_12.TaskC;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * Created by Лёша on 20.09.2016.
+ */
+public class TaskC3 {
+    {
+        System.out.println("***TaskC3");
+        String correct = "((){}[()])";
+        String wrong = "({}[({)])";
+        System.out.println("Правильный вариант:" + correct);
+        TaskC3.checkBrackets(correct);
+        System.out.println("Неправильный вариант:" + wrong);
+        TaskC3.checkBrackets(wrong);
+    }
+
+    /**
+     * Метод служит для определения корректности расстановки скобок в выражениях
+     * @param expression входное выражение типа String
+     */
+    public static void checkBrackets(String expression){
+        // список вариантов скобок используемых в выражении
+        String variants = "({[]})";
+        // создаём список вариантов корректного закрытия скобок
+        ArrayList<String> combination = new ArrayList<>();
+        combination.add("()");
+        combination.add("{}");
+        combination.add("[]");
+        // создаём стек для хранения последовательности скобок
+        Stack<String> stack = new Stack<>();
+        // проходимся по символам переданного в метод выражения
+        for (int i = 0; i < expression.length(); i++) {
+            String charValue = String.valueOf(expression.charAt(i)); // приводим i-ый символ к строке
+            // проверяем является ли символ вариантом скобок
+            if (variants.contains(charValue)){
+                // проверяем не пустой ли наш stack, если пустой то добавляем в него скобку
+                if (!stack.isEmpty()){
+                    // проверяем образуют ли последний добавленный элемент в стеке и
+                    // i-ая скобка из переданного выражения комбинацию закрытых скобок,
+                    // которые определены а списке combination
+                    if (combination.contains(stack.peek()+charValue)){
+                        stack.pop(); // если содержатся, то удаляем последний элемента steck
+                    } else {
+                        stack.add(charValue); // если нет, то добавляем скобку в стек
+                    }
+                }else {
+                    stack.add(charValue);
+                }
+            }
+        }
+        // если скобок в stack не осталось, значит каждая открывающая скобка имела закрывающую
+        // иначе скобки были расставленны некоректно
+        if (stack.isEmpty()){
+            System.out.println("Скобки раставленны верно");
+        } else {
+            System.out.println("Скобки расставленны не верно");
+        }
+    }
+    }
+
