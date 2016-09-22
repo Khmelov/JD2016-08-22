d865f728c5094655e770a136fd30fed145f600c1
Sergey-Dubatovka <sssergey83@gmail.com>
2016-09-22 21:22:19 +0300

    Коллекции B2

--- src/by/it/dubatovka/jd01_12/Run12.java
+++ src/by/it/dubatovka/jd01_12/Run12.java
@@ -1,6 +1,7 @@
 package by.it.dubatovka.jd01_12;
 
 import by.it.akhmelev.jd01_06.Level_A.A1;
+import by.it.voronovich.JD01_06.TaskB2;
 
 /**
  * Created by Dubatovka Sergey on 21.09.2016.
@@ -11,7 +12,7 @@ public class Run12 {
         Task_A2.getUnionGetCrossAB();
         Task_A3.sortPositiveNegative();
         Task_B1.taskb1(TextB1.jingle_Bells);
-
+        Task_B2.tb2(10000);
 
     }
 }
