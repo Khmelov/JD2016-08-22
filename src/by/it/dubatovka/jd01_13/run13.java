cddee829981873cb33e48464772b4f74dcdf8361
Sergey-Dubatovka <sssergey83@gmail.com>
2016-09-23 17:00:35 +0300

    Exceptions A

---
+++ src/by/it/dubatovka/jd01_13/run13.java
@@ -0,0 +1,10 @@
+package by.it.dubatovka.jd01_13;
+
+/**
+ * Created by Dubatovka Sergey on 23.09.2016.
+ */
+public class run13 {
+    public static void main(String[] args) {
+        TaskA_1.aException();
+    }
+}
