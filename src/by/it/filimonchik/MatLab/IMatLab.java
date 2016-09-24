cb43745e81b6e72d00e04ec19624131c2996a679
Raman.Filimonchyk <lekardre@mail.ru>
2016-09-25 02:00:13 +0300

    MatLab

---
+++ src/by/it/filimonchik/MatLab/IMatLab.java
@@ -0,0 +1,11 @@
+package by.it.filimonchik.MatLab;
+
+/**
+ * Created by Raman.Filimonchyk on 22/09/2016.
+ */
+interface IMatLab {
+    Variable add(Variable varAdd);
+    Variable sub(Variable varSub);
+    Variable mul(Variable varMul);
+    Variable div(Variable varDiv);
+}
