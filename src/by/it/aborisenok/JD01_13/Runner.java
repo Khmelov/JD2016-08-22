ef2cc87050d31138bb1538ae90ab05ba72bfcc8e
aborisenok <lesha_borisenok@mail.ru>
2016-09-24 15:12:38 +0300

    Task 13 A

---
+++ src/by/it/aborisenok/JD01_13/Runner.java
@@ -0,0 +1,12 @@
+package by.it.aborisenok.JD01_13;
+
+
+
+/**
+ * Created by Лёша on 24.09.2016.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        TaskA.run();
+    }
+}
---
+++ src/by/it/aborisenok/JD01_13/TaskB.java
@@ -0,0 +1,7 @@
+package by.it.aborisenok.JD01_13;
+
+/**
+ * Created by Лёша on 24.09.2016.
+ */
+public class TaskB {
+}
