5568ee321437c6113a3d698142bd6a64f3ee352f
snegurskiy.nn <advocate.snn@tut.by>
2016-09-25 22:38:05 +0300

    добавил 15a b

---
+++ src/by/it/snegurskiy/jd01_15/Runner15.java
@@ -0,0 +1,14 @@
+package by.it.snegurskiy.jd01_15;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Created by snegurskiy.nn on 23.09.2016.
+ */
+public class Runner15 {
+    public static void main(String[] args) throws FileNotFoundException {
+//        A15.A1();
+//        B15.B1();
+
+    }
+}
