57e86cbd8b7f072ef7561a611837dca9d483704c
Sergey-Dubatovka <sssergey83@gmail.com>
2016-09-26 02:36:52 +0300

    "JD01_15 Потоки ввода-вывода" A

---
+++ src/by/it/dubatovka/jd01_15/Run15.java
@@ -0,0 +1,12 @@
+package by.it.dubatovka.jd01_15;
+
+import java.io.IOException;
+
+public class Run15 {
+    public static void main(String[] args) throws IOException{
+
+
+        TaskA.taskA();
+    }
+
+}
