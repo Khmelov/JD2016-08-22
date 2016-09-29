8fd664d33a8a826ba72deb7d85e41981f5809ed2
duch <shkantau@gmail.com>
2016-09-29 08:50:43 +0300

    start jd02_02

--- src/by/it/shkantau/jd02_01/IBuyer.java
+++ src/by/it/shkantau/jd02_01/IBuyer.java
@@ -2,9 +2,11 @@ package by.it.shkantau.jd02_01;
 
 
  interface IBuyer {
+     void enterToMarket();
+     void chooseGoods();
+     void waitService();   // Встал в очередь, ожидает обслуживания
+     void goToOut();
 
-    void enterToMarket();
-    void chooseGoods();
-    void goToOut();
+
 
 }
--- src/by/it/shkantau/jd02_01/Runner.java
+++ src/by/it/shkantau/jd02_01/Runner.java
@@ -4,8 +4,12 @@ public class Runner {
 
     private static int countSeconds = 0;
     private static int countBuyer = 0;
+    protected volatile static int counterCashier=0;
 
     public static void main(String[] args) {
+//        Cashier cashier = new Cashier();
+//        new Thread (cashier).start();
+
         while (countSeconds++ < 10){
             try {
                 Thread.sleep(1000);
@@ -24,6 +28,17 @@ public class Runner {
                         new Thread(new Buyer(countBuyer)).start(); // simple mortal
                     }
 
+//create cash
+                    int size = 0;
+                        synchronized (Queues.buyers) {
+                            size = Queues.buyers.size();
+                        }
+
+                    if (size > 0 && counterCashier < 5) {
+                        Cashier cashier1 = new Cashier(Integer.toString(counterCashier));
+                        new Thread(cashier1).start();
+                        counterCashier++;
+                    }
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
