47bdf3690676ba6fa2d8a155598602560c15863f
snegurskiy.nn <advocate.snn@tut.by>
2016-10-02 22:28:42 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/by/it/snegurskiy/jd01_15/C15.java
    #	src/by/it/snegurskiy/jd01_15/Runner15.java

--- src/by/it/snegurskiy/jd02_01/Buyer.java
+++ src/by/it/snegurskiy/jd02_01/Buyer.java
@@ -1,95 +1,65 @@
 package by.it.snegurskiy.jd02_01;
 
-import java.util.HashMap;
-
 /**
- * Created by snegurskiy.nn on 01.10.2016.
+ * Created by snegurskiy.nn on 26.09.2016.
  */
-public class Buyer extends Thread implements IBuyer, Runnable, IUseBasket {
+public class Buyer extends Thread implements IBuyer,IUseBasket {
 
     int number;
-              boolean pensioner=false;
-
-
 
     public Buyer(Integer number) {
-        this.number=number;
-        if (number%4==0&number!=0) pensioner=true;
-        if (pensioner==true) this.setName("Покупатель №"+number.toString()+ " пенсионер");
-        else this.setName("Покупатель №"+number.toString());
-
+        super("Покупатель №" + number.toString());
+        this.number = number;
         start();
-
     }
 
-    public void run() {
-        enterToMarket(); //вошел в магазин (мгновенно)
-        takeBasket(); //взял корзину
-        chooseGoods(); //выбрал товар (от 0,5 до 2 секунд)
-        putGoodsToBasket(); //положил выбранный товар в корзину
-        goToOut(); //отправился на выход(мгновенно)
-        Runner0201.countBuyers--;
-        System.out.println("В магазине покупателей "+Runner0201.countBuyers+"; За день покупателей  "+Runner0201.countBuyer);
-    }
+        @Override
+        public void enterToMarket () {
 
-    @Override
-    public void enterToMarket() {
-        System.out.println(this + " зашел в магазин");
+            System.out.println(this + " зашел в магазин");
 
-    }
+        }
 
-    @Override
-    public void chooseGoods() {
-        int pause=TimeHelper.random(500,2000);
-        try {
-            if (pensioner==true) {
-                sleep(pause*1,5);
-                System.out.println("медлит пенсионер:)");
+        @Override
+        public void chooseGoods () {
+            int pause=TimeHelper.random(500,2000);
+            try {
+                sleep(pause);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            else sleep(pause);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            System.out.println(this + " выбрал товар");
         }
-        System.out.println(this + " выбрал товар");
 
-    }
+        @Override
+        public void goToOut () {
+            System.out.println(this + " вышел в магазин");
 
-    @Override
-    public void goToOut() {
-        System.out.println(this + " вышел из магазина");
-
-    }
-
-    @Override
-    public String toString() {
-        return this.getName();
-    }
-
-    @Override
-    public void takeBasket() {
-        int pause=TimeHelper.random(100,200);
-        try {
-            if (pensioner==true) sleep(pause*1,5);
-            else sleep(pause);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
-        System.out.println(this + " взял корзину");
 
-    }
+        @Override
+        public void run () {
+            enterToMarket(); //вошел в магазин (мгновенно)
+            takeBasket(); //взял корзину
+            chooseGoods(); //выбрал товар (от 0,5 до 2 секунд)
+            putGoodsToBasket(); //положил выбранный товар в корзину
+            goToOut(); //отправился на выход(мгновенно)
 
-    @Override
-    public void putGoodsToBasket() {
-        int pause=TimeHelper.random(100,200);
-        try {
-            if (pensioner==true) sleep(pause*1,5);
-            else sleep(pause);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
         }
-        HashMap<String, Double> a=Basket.GoodsInBasket();
 
-        System.out.println(this + " положил в корзину "+a);
+        @Override
+        public String toString () {
+            return this.getName();
+        }
 
+        @Override
+        public void takeBasket () {
+            System.out.println(this + " взял корзинку");
+        }
+
+        @Override
+        public void putGoodsToBasket () {
+            System.out.println(this + " положил товар в корзинку");
+        }
     }
-}
