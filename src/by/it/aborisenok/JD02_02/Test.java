b7cc04532e18ec34fc32b9503fb10c060b2ad740
aborisenok <lesha_borisenok@mail.ru>
2016-10-03 20:58:39 +0300

    Работаем над магазином

---
+++ src/by/it/aborisenok/JD02_02/Test.java
@@ -0,0 +1,14 @@
+package by.it.aborisenok.JD02_02;
+
+/**
+ * Created by aborisenok on 29.09.2016.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("|----------|");
+        String s = "Hello";
+        int i = 8;
+        int j = 6;
+        System.out.println(String.format("%s",new String(i + " d" + j)) );
+    }
+}
