5b93d92e7794ccd04f7c52d794a1b8ec8c5403bd
ddvoron <voronjava@mail.ru>
2016-10-03 15:53:12 +0300

    МНОГОПОТОЧНОСТЬ

---
+++ src/by/it/voronovich/JD02_03/Programme_Version/Interface/IUseBasket.java
@@ -0,0 +1,20 @@
+package by.it.voronovich.JD02_03.Programme_Version.Interface;
+
+/**
+ * Интерфейс реализует допонительные методы покупателей
+ *
+ * @author Voronovich Dmitry
+ * @version 1.0
+ */
+public interface IUseBasket {
+
+    /**
+     * взять корзину для покупок
+     */
+    void takeBasket();
+
+    /**
+     * положить покупки в корзину
+     */
+    void putGoodsIntoBasket();
+}
---
+++ src/by/it/voronovich/JD02_03/Programme_Version/Util/Queue.java
@@ -0,0 +1,21 @@
+package by.it.voronovich.JD02_03.Programme_Version.Util;
+
+import by.it.voronovich.JD02_03.Programme_Version.Client;
+
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+/**
+ * Данный класс добавляет в синхронизированную очередь клиентов
+ *
+ * @author Voronovich Dmitry
+ * @version 1.0
+ */
+public class Queue {
+
+    public static ConcurrentLinkedDeque<Client> queue = new ConcurrentLinkedDeque<Client>() {
+        @Override
+        public void addLast(Client client) {
+            super.addLast(client);
+        }
+    };
+}
