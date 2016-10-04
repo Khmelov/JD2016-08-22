75af0741f1a10e640c615f86dbb2ec991e190a02
Evgeniy Grechishnikov <ke6a93@gmail.com>
2016-10-04 22:46:30 +0300

    jd02_03

--- src/by/it/grechishnikov/matLab/controller/Runner.java
+++ src/by/it/grechishnikov/matLab/controller/Runner.java
@@ -9,22 +9,20 @@ public class Runner {
     public static LinkedHashMap<String, Var> storage = new LinkedHashMap<>();
     private static IPrintable printer;
     private static IReadable reader;
-    public static Operation operation;
     private static String path;
 
     public static void main(String[] args) {
         init();
-        reader.deserializeMap(path); //загружаем сохраненные переменные в карту
+//        reader.deserializeMap(path); //загружаем сохраненные переменные в карту
         Parser parser = new Parser(reader);
         parser.run();
         printer.printMap(); //выводим карту на экран
-        printer.serializeMap(path); //сохраняем переменные из карты
+//        printer.serializeMap(path); //сохраняем переменные из карты
     }
 
     public static void init() {
         path = System.getProperty("user.dir") + "/src/by/it/grechishnikov/matlab/model/vars.txt";
         printer = new ConsolePrinterImplIprintable();
         reader = new ReaderFromConsoleImplIReadable();
-        operation = new Operation();
     }
 }
