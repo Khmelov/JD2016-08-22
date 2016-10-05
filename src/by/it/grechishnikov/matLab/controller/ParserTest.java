0918581d5d24ec5c76c722dfdf3d0bc12fac0284
Evgeniy Grechishnikov <ke6a93@gmail.com>
2016-10-05 14:00:39 +0300

    matlab fix

--- src/by/it/grechishnikov/matLab/controller/ParserTest.java
+++ src/by/it/grechishnikov/matLab/controller/ParserTest.java
@@ -1,6 +1,5 @@
 package by.it.grechishnikov.matLab.controller;
 
-import by.it.grechishnikov.matLab.model.*;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,28 +9,49 @@ public class ParserTest {
     @Test
     public void parseAdd() throws Exception {
         String text = "A=2+5.3";
-        Var var = new Parser(new ReaderFromConsoleImplIReadable()).parse(text);
-        assertEquals(var.toString(), "A = 7.3");
+        String test = new Parser(new ReaderFromConsoleImplIReadable()).run(text);
+        assertEquals(test, "A = 7.3");
     }
 
     @Test
     public void parseMul() throws Exception {
         String text = "B=A*3.5";
-        Var var = new Parser(new ReaderFromConsoleImplIReadable()).parse(text);
-        assertEquals(var.toString(), "B = 25.55");
+        String test = new Parser(new ReaderFromConsoleImplIReadable()).run(text);
+        assertEquals(test, "B = 25.55");
     }
 
     @Test
     public void parseSub() throws Exception {
         String text = "B1=B-0.55";
-        Var var = new Parser(new ReaderFromConsoleImplIReadable()).parse(text);
-        assertEquals(var.toString(), "B1 = 25.0");
+        String test = new Parser(new ReaderFromConsoleImplIReadable()).run(text);
+        assertEquals(test, "B1 = 25.0");
     }
 
     @Test
     public void parseDiv() throws Exception {
         String text = "B2=A/2";
-        Var var = new Parser(new ReaderFromConsoleImplIReadable()).parse(text);
-        assertEquals(var.toString(), "B2 = 3.65");
+        String test = new Parser(new ReaderFromConsoleImplIReadable()).run(text);
+        assertEquals(test, "B2 = 3.65");
+    }
+
+    @Test
+    public void test1() throws Exception {
+        String text = "F=1-5.0*9.0";
+        String test = new Parser(new ReaderFromConsoleImplIReadable()).run(text);
+        assertEquals(test, "F = -44");
+    }
+
+    @Test
+    public void test2() throws Exception {
+        String text = "F=20/2.0*100+200.0";
+        String test = new Parser(new ReaderFromConsoleImplIReadable()).run(text);
+        assertEquals(test, "F = 1200.0");
+    }
+
+    @Test
+    public void test3() throws Exception {
+        String text = "F=3*3*3";
+        String test = new Parser(new ReaderFromConsoleImplIReadable()).run(text);
+        assertEquals(test, "F = 27.0");
     }
 }
