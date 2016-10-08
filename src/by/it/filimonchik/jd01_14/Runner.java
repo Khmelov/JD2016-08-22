15d7a281c87cb688ac60d66686b86c8c918f4224
Raman.Filimonchyk <lekardre@mail.ru>
2016-10-08 18:18:07 +0300

    jd01_14. Задания А и B

---
+++ src/by/it/filimonchik/jd01_14/Runner.java
@@ -0,0 +1,15 @@
+package by.it.filimonchik.jd01_14;
+
+import java.io.IOException;
+
+/**
+ * Created by Raman.Filimonchyk on 26/09/2016.
+ */
+public class Runner {
+    public static void main (String [] args) throws Exception {
+        WriteInTheFile.A1();
+        FileRead.A2();
+        Average.A3();
+        ScanText.B();
+    }
+}
