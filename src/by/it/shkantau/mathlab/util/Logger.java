1781dbdbaf10008ff4f0963e9a221c102cc70d35
Aliaksei <shkantau@gmail.com>
2016-10-09 23:38:27 +0300

    add to mathlab Logger (singleton)

--- src/by/it/shkantau/mathlab/util/Logger.java
+++ src/by/it/shkantau/mathlab/util/Logger.java
@@ -3,33 +3,29 @@ package by.it.shkantau.mathlab.util;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Date;
 
-public class Logger {
+class Logger {
 
-    private static Logger loggerInstance;
+    private static Logger loggerInctance;
 
-    private String path ;
-    private File logFile;
+    private String path = System.getProperty("user.dir") + "\\src\\by\\it\\shkantau\\mathlab\\mathLab.log";
+    private File logFile = new File(path);
 
-    private Logger(String path) {
-        this.path = path;
-        this.logFile = new File(path);
+    private Logger() {
     }
 
-    public static Logger getLogger(String path){
-        if (loggerInstance == null){
-            loggerInstance = new Logger(path);
+    public static Logger getLogger(){
+        if (loggerInctance == null){
+            loggerInctance = new Logger();
         }
-        return loggerInstance;
+        return loggerInctance;
     }
 
-    public void print(String string) {
-        try (FileWriter fw = new FileWriter(logFile, true)){
-            fw.write(new Date() +"\t" + string+"\n");
+    void print(String string) {
+        try (FileWriter fw = new FileWriter(logFile)){
+            fw.write(string);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
