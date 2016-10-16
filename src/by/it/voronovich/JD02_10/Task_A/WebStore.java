d68e59132094a8c275488bd7f51df04073a1a137
ddvoron <dd.versus@gmail.com>
2016-10-16 23:41:13 +0300

    JD02_10 A, B

---
+++ src/by/it/voronovich/JD02_10/Task_A/WebStore.java
@@ -0,0 +1,48 @@
+package by.it.voronovich.JD02_10.Task_A;
+
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "webStore", propOrder = {
+        "clients"
+})
+
+public class WebStore {
+
+    @XmlElement(required = true)
+    private Clients clients = new Clients();
+
+    //конструктор без параметров
+    public WebStore() {
+    }
+
+    public WebStore(Clients clients) {
+        this.clients = clients;
+    }
+
+    public Clients getClients() {
+        return clients;
+    }
+
+    public void setClients(Clients clients) {
+        this.clients = clients;
+    }
+    //преобразование в строку
+    @Override
+    public String toString() {
+        return "Список клиентов" + clients;
+    }
+
+    //сравнение
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    //хэшкод
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
