3707859d6451affe5673615cdc832594871b437f
ddvoron <dd.versus@gmail.com>
2016-10-16 18:21:11 +0300

    JD02_09

--- src/by/it/voronovich/JD02_09/Task_B/JAXB_toXML.java
+++ src/by/it/voronovich/JD02_09/Task_B/JAXB_toXML.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class JAXB_toXML {
     public static void main(String[] args) {
-        //creating Room objects
+
         ArrayList<Client> list = new ArrayList<>();
         Client client = new Client("Derek", "Serx");
         list.add(client);
@@ -18,23 +18,19 @@ public class JAXB_toXML {
         list.add(client);
         client = new Client("Coni", "Demiko");
         list.add(client);
-        //creating Rooms object
+
         Clients clients = new Clients();
         clients.setList(list);
-        //creating Hotel object
+
         WebStore webStore = new WebStore();
         webStore.setClients(clients);
 
         try {
-            // create JAXB context and initializing Marshaller
             JAXBContext context = JAXBContext.newInstance(WebStore.class);
             Marshaller marshaller = context.createMarshaller();
-            //specify the location and name of xml file to be created
             File file = new File("src/by/it/voronovich/JD02_09/Task_B/WebStore.xml");
-            // writing to XML file
             marshaller.marshal(webStore, new FileOutputStream(file));
             System.out.println("XML-файл создан");
-            // writing to console
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
             marshaller.marshal(webStore, System.out);
         } catch (FileNotFoundException e) {
