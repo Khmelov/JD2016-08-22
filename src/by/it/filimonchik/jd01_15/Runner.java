53b282354a7d980bfafde2593fc57fe940ae96af
Raman.Filimonchyk <lekardre@mail.ru>
2016-10-18 23:04:20 +0300

    jd02_05 A, B, C

---
+++ src/by/it/filimonchik/jd01_15/Runner.java
@@ -0,0 +1,10 @@
+package by.it.filimonchik.jd01_15;
+
+/**
+ * Created by Raman.Filimonchyk on 18/10/2016.
+ */
+public class Runner {
+    public static void main (String [] args) throws  Exception{
+        Task_B.B1();
+    }
+}
---
+++ src/by/it/filimonchik/jd01_15/Task_A.java
@@ -0,0 +1,7 @@
+package by.it.filimonchik.jd01_15;
+
+/**
+ * Created by Raman.Filimonchyk on 18/10/2016.
+ */
+public class Task_A {
+}
