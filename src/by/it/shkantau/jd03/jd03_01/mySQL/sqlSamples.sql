54c23694c0911bea55dfdc7bb522c720afb71cbb
Aliaksei <shkantau@gmail.com>
2016-10-19 16:37:06 +0300

    start jd03_01 create base airport  by MySQL Workbench

---
+++ src/by/it/shkantau/jd03/jd03_01/mySQL/sqlSamples.sql
@@ -0,0 +1,12 @@
+--insert into users(login,pass,role) values('admin','admin',1);
+--insert into users(login,pass,role) values('dispatcher','dispatcher',2);
+
+--insert into roles(`roleType`) values ('admimistrator');
+--insert into roles(`roleType`) values ('dispatcher');
+--insert into roles(`roleType`) values ('user');
+
+--select * from roles;
+--delete from roles WHERE ID BETWEEN 4 and 9;
+--delete from roles; --clear role table
+
+select * from roles;
