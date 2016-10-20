6714a9a05f8f67260a2fcbdb6aeef401e787a249
Evgeniy Grechishnikov <ke6a93@gmail.com>
2016-10-20 12:33:54 +0300

    jd03_01 Task C, jd03_02 Task A, B

---
+++ src/by/it/grechishnikov/jd03_02/Connection.java
@@ -0,0 +1,21 @@
+package by.it.grechishnikov.jd03_02;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Connection {
+    public static java.sql.Connection connection;
+    public static Statement statement;
+
+    static {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Store" +
+                            "?useUnicode=true&amp;characterEncoding=UTF-8",
+                    "root", "");
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/by/it/grechishnikov/jd03_02/crud/User.java
@@ -0,0 +1,63 @@
+package by.it.grechishnikov.jd03_02.crud;
+
+public class User {
+    private String login;
+    private String password;
+    private String email;
+    private int role;
+    private int id;
+
+    public User() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", role=" + role +
+                ", id=" + id +
+                '}';
+    }
+}
