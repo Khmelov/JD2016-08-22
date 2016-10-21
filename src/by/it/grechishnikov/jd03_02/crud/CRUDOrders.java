a0e290a5c7c854cb5276ae7f28c5caf5caeae96b
Evgeniy Grechishnikov <ke6a93@gmail.com>
2016-10-21 14:58:43 +0300

    jd03_02 C

--- src/by/it/grechishnikov/jd03_02/crud/CRUDOrders.java
+++ src/by/it/grechishnikov/jd03_02/crud/CRUDOrders.java
@@ -1,15 +1,13 @@
 package by.it.grechishnikov.jd03_02.crud;
 
-import by.it.grechishnikov.jd03_02.Connection;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+
+import static by.it.grechishnikov.jd03_02.Connection.statement;
 
 public class CRUDOrders {
     public static void create(Order order) {
         try {
-            Statement statement = Connection.startConnection();
             statement.executeUpdate(String.format("insert into orderList(FK_User,FK_Goods) values('%d','%d')",
                     order.getUser(), order.getGoods()));
             ResultSet set = statement.executeQuery(String.format("select id from orderList where FK_User = '%d' and " +
@@ -19,14 +17,11 @@ public class CRUDOrders {
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            Connection.closeConnection();
         }
     }
 
     public static Order read(int id) {
         try {
-            Statement statement = Connection.startConnection();
             ResultSet set = statement.executeQuery(String.format("select * from orderList where id = '%d'", id));
             set.next();
             return new Order(
@@ -35,20 +30,15 @@ public class CRUDOrders {
                     set.getInt("FK_Goods"));
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            Connection.closeConnection();
         }
         return null;
     }
 
     public static boolean delete(int id) {
         try {
-            Statement statement = Connection.startConnection();
             return statement.executeUpdate(String.format("delete from orderList where id = %d", id)) == 1;
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            Connection.closeConnection();
         }
         return false;
     }
