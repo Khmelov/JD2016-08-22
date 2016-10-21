39463016f12a9cf8bae27869b2db3ed083f4669a
aborisenok <lesha_borisenok@mail.ru>
2016-10-21 19:59:45 +0300

    JD03_03

---
+++ src/by/it/aborisenok/JD03_02/crud/Talon.java
@@ -0,0 +1,12 @@
+package by.it.aborisenok.JD03_02.crud;
+
+/**
+ * Created by Лёша on 21.10.2016.
+ */
+public class Talon {
+    private int ID;
+    private int FK_Policlinic;
+    private int FK_Pacient;
+    private int FK_Doctor;
+    private int FK_Date;
+}
