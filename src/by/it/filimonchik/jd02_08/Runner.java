2b541e48240b9bda2c891340d9db9cbb97c09542
Raman.Filimonchyk <lekardre@mail.ru>
2016-10-21 17:51:15 +0300

    jd02_08 A

---
+++ src/by/it/filimonchik/jd02_08/Runner.java
@@ -0,0 +1,13 @@
+package by.it.filimonchik.jd02_08;
+
+import org.xml.sax.SAXException;
+
+/**
+ * Created by Raman.Filimonchyk on 21/10/2016.
+ */
+public class Runner {
+    public static void main (String [] args) throws SAXException {
+        System.out.println("Задание А.");
+        SAX.Task_A_SAX();
+    }
+}
