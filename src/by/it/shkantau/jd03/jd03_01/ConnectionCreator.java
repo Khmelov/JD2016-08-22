b434a09ad8f280947ef4e5aac29e105a14882571
Aliaksei <shkantau@gmail.com>
2016-10-24 00:08:45 +0300

    ad jd03_02+

--- src/by/it/shkantau/jd03/jd03_01/ConnectionCreator.java
+++ src/by/it/shkantau/jd03/jd03_01/ConnectionCreator.java
@@ -2,7 +2,6 @@ package by.it.shkantau.jd03.jd03_01;
 
 import com.google.gson.Gson;
 import com.mysql.fabric.jdbc.FabricMySQLDriver;
-import com.sun.jmx.snmp.agent.SnmpUserDataFactory;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -14,30 +13,28 @@ import java.sql.SQLException;
 
 public class ConnectionCreator {
 
+    private static volatile Connection connection = null;
+    private final static File file = new File(System.getProperty("user.dir") + "/src/by/it/shkantau/jd03/jd03_01/CSettings.json");
+    private static ConnectionSettingsPOJO connectionSettings;
+
+
     static {
         Driver driver;
         try {
             driver = new FabricMySQLDriver();
             DriverManager.registerDriver(driver);
         } catch (SQLException e) {
-            System.out.println("error: not register driver");
+            System.out.println(e.getLocalizedMessage());
         }
-
     }
 
-    private static volatile Connection connection = null;
-    private final static File file = new File(System.getProperty("user.dir") + "/src/by/it/shkantau/jd03/jd03_01/CSettings.json");
-    private static ConnectionSettingsPOJO cs;
-
-    Gson gson = new Gson();
-
     public static Connection getConnection() throws SQLException, FileNotFoundException {
-         cs = (new Gson()).fromJson(new FileReader(file), ConnectionSettingsPOJO.class);
+         connectionSettings = (new Gson()).fromJson(new FileReader(file), ConnectionSettingsPOJO.class);
 
         if (connection == null || connection.isClosed()) {
                 synchronized (ConnectionSettingsPOJO.lock) {
                     if (connection == null || connection.isClosed())
-                        connection = DriverManager.getConnection(cs.getURL_DB(), cs.getUSER_DB(), cs.getPASSWORD_DB());
+                        connection = DriverManager.getConnection(connectionSettings.getURL_DB(), connectionSettings.getUSER_DB(), connectionSettings.getPASSWORD_DB());
                 }
         }
         return connection;
