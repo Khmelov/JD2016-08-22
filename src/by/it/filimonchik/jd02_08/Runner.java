f823c2c3416e3d7d886afed247a6cedc6b02dae8
Raman.Filimonchyk <lekardre@mail.ru>
2016-10-23 12:43:17 +0300

    jd02_08 A, B, C

--- src/by/it/filimonchik/jd02_08/Runner.java
+++ src/by/it/filimonchik/jd02_08/Runner.java
@@ -2,12 +2,20 @@ package by.it.filimonchik.jd02_08;
 
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
+import java.io.IOException;
+
 /**
  * Created by Raman.Filimonchyk on 21/10/2016.
  */
 public class Runner {
-    public static void main (String [] args) throws SAXException {
+    public static void main (String [] args) throws SAXException, IOException, XMLStreamException, ParserConfigurationException {
         System.out.println("Задание А.");
         SAX.Task_A_SAX();
+        System.out.println("Задание B.");
+        StaX.Task_B_StAX();
+        System.out.println("Задание C.");
+        DOM.Task_C_DOM();
     }
 }
