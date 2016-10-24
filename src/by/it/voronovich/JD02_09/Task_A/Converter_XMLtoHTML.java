bc372f0dbc035851fad62aec7cd93262a5afdbe5
Snenn <advocate.snn@tut.by>
2016-10-24 12:37:17 +0300

    24.10

--- src/by/it/voronovich/JD02_09/Task_A/Converter_XMLtoHTML.java
+++ src/by/it/voronovich/JD02_09/Task_A/Converter_XMLtoHTML.java
@@ -9,20 +9,20 @@ import java.io.OutputStream;
 
 public class Converter_XMLtoHTML {
 
-    public static void main(String args[]) {
-        try {
-            TransformerFactory tFactory = TransformerFactory.newInstance();
-
-            Source xslDoc = new StreamSource("src/by/it/voronovich/JD02_09/Task_A/WebStore.xsl");
-            Source xmlDoc = new StreamSource("src/by/it/voronovich/JD02_07/WebStore.xml");
-
-            String outputFileName = "src/by/it/voronovich/JD02_09/Task_A/WebStore.html";
-
-            OutputStream htmlFile = new FileOutputStream(outputFileName);
-            Transformer trasform = tFactory.newTransformer(xslDoc);
-            trasform.transform(xmlDoc, new StreamResult(htmlFile));
-        } catch (FileNotFoundException | TransformerFactoryConfigurationError | TransformerException e) {
-            e.printStackTrace();
-        }
-    }
+//    public static void main(String args[]) {
+//        try {
+//            TransformerFactory tFactory = TransformerFactory.newInstance();
+//
+//            Source xslDoc = new StreamSource("src/by/it/voronovich/JD02_09/Task_A/WebStore.xsl");
+//            Source xmlDoc = new StreamSource("src/by/it/voronovich/JD02_07/WebStore.xml");
+//
+//            String outputFileName = "src/by/it/voronovich/JD02_09/Task_A/WebStore.html";
+//
+//            OutputStream htmlFile = new FileOutputStream(outputFileName);
+//            Transformer trasform = tFactory.newTransformer(xslDoc);
+//            trasform.transform(xmlDoc, new StreamResult(htmlFile));
+//        } catch (FileNotFoundException | TransformerFactoryConfigurationError | TransformerException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
