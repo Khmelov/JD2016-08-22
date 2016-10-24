ee311901ca77770aff3f33540e02aa2f0612cc4a
Snenn <advocate.snn@tut.by>
2016-10-24 12:34:44 +0300

    24.10

---
+++ src/by/it/snegurskiy/jd02_09/XMLtoHTML.java
@@ -0,0 +1,30 @@
+package by.it.snegurskiy.jd02_09;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+/**
+ * Created by Acer on 24.10.2016.
+ */
+public class XMLtoHTML {
+    final static String root="src/by/it/snegurskiy/jd02_09/";
+    public static void main(String[ ] args) {
+        String fileName=root+"user+xsd.xml";
+        String fileHTML=root+"xpath.html";
+
+        try {
+            TransformerFactory tf = TransformerFactory.newInstance();
+            // установка используемого XSL-преобразования
+            Transformer transformer = tf.newTransformer(new StreamSource(root+"xpath.xsl"));
+            // установка исходного XML-документа и конечного XML-файла
+            transformer.transform(new StreamSource(fileName),
+                    new StreamResult(fileHTML));
+            System.out.println("Transform " + fileName + " complete");
+        } catch(TransformerException e) {
+            System.err.println("Impossible transform file " + fileName + " : " + e);
+        }
+    }
+}
