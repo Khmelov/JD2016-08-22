cb9d7f4957213a372aa658a23c89e8e4f9055d0d
Snenn <advocate.snn@tut.by>
2016-10-26 11:35:43 +0300

    24.10

--- src/by/it/snegurskiy/jd02_09/XML2JAXB.java
+++ src/by/it/snegurskiy/jd02_09/XML2JAXB.java
@@ -1,24 +1,26 @@
 package by.it.snegurskiy.jd02_09;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
-//public class XML2JAXB {
-//    public static void main(String[] args) {
-//        try {
-//            JAXBContext jaxbContext   = JAXBContext.newInstance(Users.class);
-//            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-//
-//            String fileName   = "src/by/it/snegurskiy/jd02_09/user+xsd.xml";
-//            FileReader reader = new FileReader(fileName);
-//
-//            System.out.println("XML прочитан.");
-//            Users user = (Users) unmarshaller.unmarshal(reader);
-//
-//            System.out.println(user);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+public class XML2JAXB {
+    public static void main(String[ ] args) {
+        try {
+            JAXBContext jaxbContext   = JAXBContext.newInstance(User.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+            String fileName   = "src/by/it/snegurskiy/jd02/jd02_09/user+xsd.xml";
+            FileReader reader = new FileReader(fileName);
+
+            System.out.println("XML прочитан.");
+            User user = (User) unmarshaller.unmarshal(reader);
+
+            System.out.println(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
