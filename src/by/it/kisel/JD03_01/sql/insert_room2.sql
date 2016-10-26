193150e71af02e352f82eb22fd34b061776734db
EvgenyKisel <kisel.jenya@gmail.com>
2016-10-27 01:46:17 +0300

    JD03_01 добавление

---
+++ src/by/it/kisel/JD03_01/sql/insert_room2.sql
@@ -0,0 +1,5 @@
+INSERT INTO rooms (
+ArrivalDate, DepartureDate, RoomCount, Quality,
+	Price, Сonditioner, FK_persons, Description
+)
+VALUES ('01.11', '13.11', 4, 10, 2000, 'Yes', 11, 'Elite room');
