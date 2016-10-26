80bd321089bc10cad0c62582f44dc0d126bf4f02
Snenn <advocate.snn@tut.by>
2016-10-26 12:09:57 +0300

    24.10

---
+++ src/by/it/snegurskiy/jd02_09/generate/Users.java
@@ -0,0 +1,25 @@
+package by.it.snegurskiy.jd02_09.generate;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+
+@XmlRootElement
+public class Users {
+    @XmlElement(name="user")
+    private ArrayList<User> list = new ArrayList<User>();
+    public Users() {
+        super();
+    }
+    public void setList(ArrayList<User> list) {
+        this.list = list;
+    }
+    public boolean add(User user) {
+        return list.add(user);
+    }
+    @Override
+    public String toString() {
+        return "Students [list=" + list + "]";
+    }
+}
+
---
+++ src/by/it/snegurskiy/jd02_09/generate/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://generate.jd02_09.snegurskiy.it.by/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package by.it.snegurskiy.jd02_09.generate;
