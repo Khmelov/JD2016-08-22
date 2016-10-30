db3b4ab12f917eb8b04644f1875b7ec2537c0e2a
Raman.Filimonchyk <lekardre@mail.ru>
2016-10-31 02:47:59 +0300

    jd01_12 A, B

---
+++ src/by/it/filimonchik/jd01_12/Text.java
@@ -0,0 +1,94 @@
+package by.it.filimonchik.jd01_12;
+
+/**
+ * Created by Raman.Filimonchyk on 31/10/2016.
+ */
+public class Text {
+    public static  String threelittlepigs = "There was an old sow with three little pigs, and as she had not enough to keep them, she sent them out to seek their fortune. The first that went off met a man with a bundle of straw, and said to him:\n" +
+            "\n" +
+            "“Please, man, give me that straw to build me a house.”\n" +
+            "\n" +
+            "Which the man did, and the little pig built a house with it. Presently came along a wolf, and knocked at the door, and said:\n" +
+            "\n" +
+            "“Little pig, little pig, let me come in.”\n" +
+            "\n" +
+            "To which the pig answered:\n" +
+            "\n" +
+            "“No, no, by the hair of my chiny chin chin.”\n" +
+            "\n" +
+            "The wolf then answered to that:\n" +
+            "\n" +
+            "“Then I’ll huff, and I’ll puff, and I’ll blow your house in.”\n" +
+            "\n" +
+            "So he huffed, and he puffed, and he blew his house in, and ate up the little pig.\n" +
+            "\n" +
+            "The second little pig met a man with a bundle of furze, and said:\n" +
+            "\n" +
+            "“Please, man, give me that furze to build a house.”\n" +
+            "\n" +
+            "Which the man did, and the pig built his house. Then along came the wolf, and said:\n" +
+            "\n" +
+            "“Little pig, little pig, let me come in.”\n" +
+            "\n" +
+            "“No, no, by the hair of my chiny chin chin.”\n" +
+            "\n" +
+            "“Then I’ll puff, and I’ll huff, and I’ll blow your house in.”\n" +
+            "\n" +
+            "So he huffed, and he puffed, and he puffed, and he huffed, and at last he blew the house down, and he ate up the little pig.\n" +
+            "\n" +
+            "The third little pig met a man with a load of bricks, and said:\n" +
+            "\n" +
+            "“Please, man, give me those bricks to build a house with.”\n" +
+            "\n" +
+            "So the man gave him the bricks, and he built his house with them. So the wolf came, as he did to the other little pigs, and said:\n" +
+            "\n" +
+            "“Little pig, little pig, let me come in.”\n" +
+            "\n" +
+            "“No, no, by the hair of my chiny chin chin.”\n" +
+            "\n" +
+            "“Then I’ll huff, and I’ll puff, and I’ll blow your house in.”\n" +
+            "\n" +
+            "Well, he huffed, and he puffed, and he huffed and he puffed, and he puffed and huffed; but he could not get the house down. When he found that he could not, with all his huffing and puffing, blow the house down, he said:\n" +
+            "\n" +
+            "“Little pig, I know where there is a nice field of turnips.”\n" +
+            "\n" +
+            "“Where?” said the little pig.\n" +
+            "\n" +
+            "“Oh, in Mr. Smith’s Home-field, and if you will be ready tomorrow morning I will call for you, and we will go together, and get some for dinner.”\n" +
+            "\n" +
+            "“Very well,” said the little pig, “I will be ready. What time do you mean to go?”\n" +
+            "\n" +
+            "“Oh, at six o’clock.”\n" +
+            "\n" +
+            "Well, the little pig got up at five, and got the turnips before the wolf came (which he did about six) and who said:\n" +
+            "\n" +
+            "“Little Pig, are you ready?”\n" +
+            "\n" +
+            "The little pig said: “Ready! I have been and come back again, and got a nice potful for dinner.”\n" +
+            "\n" +
+            "The wolf felt very angry at this, but thought that he would be up to the little pig somehow or other, so he said:\n" +
+            "\n" +
+            "“Little pig, I know where there is a nice apple-tree.”\n" +
+            "\n" +
+            "“Where?” said the pig.\n" +
+            "\n" +
+            "“Down at Merry-garden,” replied the wolf, “and if you will not deceive me I will come for you, at five o’clock tomorrow and get some apples.”\n" +
+            "\n" +
+            "Well, the little pig bustled up the next morning at four o’clock, and went off for the apples, hoping to get back before the wolf came; but he had further to go, and had to climb the tree, so that just as he was coming down from it, he saw the wolf coming, which, as you may suppose, frightened him very much. When the wolf came up he said:\n" +
+            "\n" +
+            "“Little pig, what! are you here before me? Are they nice apples?”\n" +
+            "\n" +
+            "“Yes, very,” said the little pig. “I will throw you down one.”\n" +
+            "\n" +
+            "And he threw it so far, that, while the wolf was gone to pick it up, the little pig jumped down and ran home. The next day the wolf came again, and said to the little pig:\n" +
+            "\n" +
+            "“Little pig, there is a fair at Shanklin this afternoon, will you go?”\n" +
+            "\n" +
+            "“Oh yes,” said the pig, “I will go; what time shall you be ready?”\n" +
+            "\n" +
+            "“At three,” said the wolf. So the little pig went off before the time as usual, and got to the fair, and bought a butter-churn, which he was going home with, when he saw the wolf coming. Then he could not tell what to do. So he got into the churn to hide, and by so doing turned it round, and it rolled down the hill with the pig in it, which frightened the wolf so much, that he ran home without going to the fair. He went to the little pig’s house, and told him how frightened he had been by a great round thing which came down the hill past him. Then the little pig said:\n" +
+            "\n" +
+            "“Hah, I frightened you, then. I had been to the fair and bought a butter-churn, and when I saw you, I got into it, and rolled down the hill.”\n" +
+            "\n" +
+            "Then the wolf was very angry indeed, and declared he would eat up the little pig, and that he would get down the chimney after him. When the little pig saw what he was about, he hung on the pot full of water, and made up a blazing fire, and, just as the wolf was coming down, took off the cover, and in fell the wolf; so the little pig put on the cover again in an instant, boiled him up, and ate him for supper, and lived happy ever afterwards.";
+}
