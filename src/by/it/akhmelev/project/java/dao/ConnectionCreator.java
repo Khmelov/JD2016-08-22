67de76be7b65092467e3cc3f65db7a0898bfa932
Alex <akhmelev@gmail.com>
2016-10-31 23:51:51 +0300

    jd03_06 classwork

--- src/by/it/akhmelev/project/java/dao/ConnectionCreator.java
+++ src/by/it/akhmelev/project/java/dao/ConnectionCreator.java
@@ -8,6 +8,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionCreator {
+    static {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //ниже способ, который работает, но ненадежно
+            //Driver driver = new FabricMySQLDriver();
+            //DriverManager.registerDriver(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     //Памятка. Корректно держать настройки соединения вне кода (!)
     private static final String URL_DB =
             "jdbc:mysql://127.0.0.1:2016/it_academy"
@@ -19,15 +31,7 @@ public class ConnectionCreator {
     private static volatile Connection connection = null;
 
     public static Connection getConnection() throws SQLException {
-        try{
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
         if (connection == null || connection.isClosed()) {
-                Driver driver = new FabricMySQLDriver();
-                DriverManager.registerDriver(driver);
                 synchronized (URL_DB) {
                     if (connection == null || connection.isClosed())
                         connection = DriverManager.getConnection(URL_DB, USER_DB, PASSWORD_DB);
